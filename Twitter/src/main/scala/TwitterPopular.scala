40f520d85f9099dac3b0e63abff70a95445fae30
dmicevski <daniel.j.micevski@gmail.com>
2015-08-03 18:08:05 +1000

    Simplified popular example

--- Twitter/src/main/scala/TwitterPopular.scala
+++ Twitter/src/main/scala/TwitterPopular.scala
@@ -5,34 +5,22 @@ import org.apache.spark.streaming.twitter._
 object TwitterPopular {
 
   def run(args: Array[String]) {
-    val filters = args
-
     val sparkConf = new SparkConf().setAppName("TwitterPopular")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
-    val stream = TwitterUtils.createStream(ssc, None, filters)
+    
+    val stream = TwitterUtils.createStream(ssc, None, args)
 
     val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
 
-    val top60Topics = hashTags.map((_, 1)).
-                      reduceByKeyAndWindow(_ + _, Seconds(60))
-                     .map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
+    val topics = hashTags.map((_, 1))
+                 .reduceByKeyAndWindow(_ + _, Seconds(10))
+                 .map{case (topic, count) => (count, topic)}
+                 .transform(_.sortByKey(false))
 
-    val top10Topics = hashTags.map((_, 1)).
-                      reduceByKeyAndWindow(_ + _, Seconds(10))
-                     .map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
-
-    top60Topics.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-    })
-
-    top10Topics.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    topics.foreachRDD(rdd => {
+      val top = rdd.take(10)
+      println("\nTop topics (%s total):".format(rdd.count()))
+      top.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
     })
 
     ssc.start()
--- Twitter/src/main/scala/TwitterSimple.scala
+++ Twitter/src/main/scala/TwitterSimple.scala
@@ -5,12 +5,10 @@ import org.apache.spark.streaming.twitter._
 object TwitterSimple {
 
   def run(args: Array[String]) {
-    val filters = args
-
     val sparkConf = new SparkConf().setAppName("TwitterSimple")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     
-    val stream = TwitterUtils.createStream(ssc, None, filters)
+    val stream = TwitterUtils.createStream(ssc, None, args)
 
     val statuses = stream.map(status => status.getText())
     statuses.print()
